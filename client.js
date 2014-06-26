34620153e5933ab32d3a4c3a2c3bb3d1af0cbbdc
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-06-26 16:21:43 -0400

    Modularised render code and resulting simpler test

--- client.js
+++ client.js
@@ -2,15 +2,24 @@
   exports.loadTime = function() {
     var request = new XMLHttpRequest();
     request.onload = function(data) {
-      var ctx = document.getElementById("canvas").getContext("2d");
       var time = JSON.parse(data.target.responseText).time;
-      var skyColor = time === "day" ? "blue" : "black";
-
-      ctx.fillStyle = skyColor;
-      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      var color = time === "day" ? "blue" : "black";
+      renderer.fillBackground(color);
     };
 
     request.open("GET", "/time.json", true);
     request.send();
+  };
+
+  var renderer = exports.renderer = {
+    ctx: function() {
+      return document.getElementById("canvas").getContext("2d");
+    },
+
+    fillBackground: function(color) {
+      var ctx = this.ctx();
+      ctx.fillStyle = color;
+      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
   };
 })(this);
--- tests/client_tests.js
+++ tests/client_tests.js
@@ -2,7 +2,7 @@ var test = require("./test");
 var client = require("../client");
 
 test(
-  "should draw blue sky when it is daytime", function() {
+  "should draw sun and blue sky in canvas when it is daytime", function() {
     global.XMLHttpRequest = function() {
       this.open = function() {};
 
@@ -13,22 +13,16 @@ test(
       };
     };
 
-    global.document = {
-      getElementById: function() {
-        return {
-          getContext: function() {
-            return {
-              canvas: { width: 300, height: 150 },
-              fillRect: function(x, y, w, h) {
-                test.isEqual(x, 0);
-                test.isEqual(y, 0);
-                test.isEqual(w, 300);
-                test.isEqual(h, 150);
-                test.isEqual(this.fillStyle, "blue");
-              }
-            };
-          }
-        };
+    client.renderer.ctx = function() {
+      return {
+        canvas: { width: 300, height: 150 },
+        fillRect: function(x, y, w, h) {
+          test.isEqual(x, 0);
+          test.isEqual(y, 0);
+          test.isEqual(w, 300);
+          test.isEqual(h, 150);
+          test.isEqual(this.fillStyle, "blue");
+        }
       }
     };
 
